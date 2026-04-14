2ff7181a37e2f956618421de5b4f4ee7ed1fb8c8
kay <kayanadonis@gmail.com>
2026-04-14 15:07:49 +0700

    feat:exercise 3.8

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/hello", (_req, res) => {
-  res.send("Hello Full Stack");
+  res.send("Hello Full Stack!");
 });
 
 app.get("/bmi", (req, res) => {
